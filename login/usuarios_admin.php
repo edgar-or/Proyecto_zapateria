53fa21c5ed86e7b68416bd84af0c80d639cb10d4
Edgar Ayala <ayalaalvarezedgarorlando@gmail.com>
2024-09-17 20:26:28 -0600

    login

--- login/usuarios_admin.php
+++ login/usuarios_admin.php
@@ -2,24 +2,38 @@
 
 include '../conexionBD.php';
 
+$usuario = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
 
-
-$usuario= $_POST['usuario'];
-$contraseña= $_POST['contraseña'];
-
-
-print("el usuario es: ".$usuario);
-print("el usuario es: ".$contraseña);
-
-$sql = "INSERT INTO usuarios_admin (nick_name, contraseña) VALUES ('$usuario', '$contraseña')";
-
-// Ejecutar la consulta
-if (mysqli_query($conn, $sql)) {
-    echo "Nuevo registro creado correctamente";
-} else {
-    echo "Error: " . mysqli_error($conn);
+// Verificar si los campos están vacíos
+if (empty($usuario) || empty($contraseña)) {
+    header("location: ../index.html");
+    exit();
 }
 
+// Crear la conexión con la base de datos
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Error al Conectar: " . mysqli_connect_error());
 
 
+if ($result->num_rows > 0) {
+    // El usuario existe, ahora verificamos la contraseña
+    $row = $result->fetch_assoc();
+    
+    if (password_verify($contraseña, $row['password'])) {
+        // Contraseña correcta, redirigir al usuario
+        header("location: ../catalogo/dama.html");
+        exit();
+    } else {
+        // Contraseña incorrecta
+        header("location: ../index.html?error=incorrect_password");
+        exit();
+    }
+} else {
+    // El usuario no existe
+    header("location: ../index.html?error=user_not_found");
+    exit();
+}
+
+// Cerrar la conexión
+$conn->close();
 ?>
