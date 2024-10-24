e54636b489a52ec26f06da5a2d91dc452d588d76
Axelolt <axel100prehappy@gmail.com>
2024-10-23 20:15:35 -0600

    AEA

--- catalogo/carrito.php
+++ catalogo/carrito.php
@@ -477,7 +477,12 @@ function mostrarTabla($productos){
             </form>
             
         </div>
-            <script src="bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+             <!-- Script de Bootstrap -->
+  <script src="../bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
         </body>
         </html>
         <?php
